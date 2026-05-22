8f32442de4b808d8ccbdf7241a18c0ab877c2485
MyJules <mudryk200@gmail.com>
2026-05-23 01:03:03 +0200

    Reorder main function in args_tests.cpp for better readability

--- tests/args_tests.cpp
+++ tests/args_tests.cpp
@@ -2,15 +2,15 @@
 
 import args;
 
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
 TEST(ArgsTests, ToSpanReturnsCorrectSize)
 {
     const char* argv[] = {"program", "arg1", "arg2"};
     const auto args_span = args::to_span(3, argv);
     EXPECT_EQ(args_span.size(), 3);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
